45f9d4320c381b63b858a4199382e397a9a064b0
Wee Weing Hwee <weinghwee.wee@hotmail.com>
2022-11-07 23:58:38 +0800

    connect to postgres db

--- src/main/java/com/example/springbootapp/model/Product.java
+++ src/main/java/com/example/springbootapp/model/Product.java
@@ -1,6 +1,15 @@
 package com.example.springbootapp.model;
 
+import javax.persistence.*;
+
+@Entity(name = "Product")
+@Table(name = "products")
 public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+
   public String getName() {
     return name;
   }
@@ -9,5 +18,11 @@ public class Product {
     this.name = name;
   }
 
-  private String name;
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
 }
--- src/main/java/com/example/springbootapp/service/ProductService.java
+++ src/main/java/com/example/springbootapp/service/ProductService.java
@@ -1,18 +1,25 @@
 package com.example.springbootapp.service;
 
 import com.example.springbootapp.model.Product;
+import com.example.springbootapp.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProductService {
+  private final ProductRepository productRepository;
+
+  public ProductService(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
   public List<Product> getProducts() {
     Product p = new Product();
     p.setName("First Product");
 
     Product p2 = new Product();
     p2.setName("Second Product");
+    productRepository.saveAll(List.of(p, p2));
     return List.of(p, p2);
   }
 }
